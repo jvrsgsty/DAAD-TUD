14b0c30435367432c3f918627e0b198939e1d111
jvrsgsty <jvrsgsty@users.noreply.github.com>
2014-11-18 15:41:30 +0100

    Version 2.2
    Added functionality to hide columns from file list table
    Added id to several form inputs in html document
    Added classes to file list <td> elements to support hide functionality
    Slight changes to css

--- Web- und Multimedia Engineering/inf_box/home/resources/inf_box-Script.js
+++ Web- und Multimedia Engineering/inf_box/home/resources/inf_box-Script.js
@@ -1,4 +1,11 @@
+/* 
+inf_box script
+Last Updated: 18.11.2014
+Author: Javier Sagastuy , Alejandro Escalante
+*/
+// 1) Sticky Nav
 $(document).ready(function(){
+
 	jQuery(document).scroll(function() {
 		if(jQuery(this).scrollTop() > 80) {
 			jQuery('.sticky-nav').addClass("is-sticky");
@@ -6,5 +13,30 @@ $(document).ready(function(){
 		else{
 			jQuery('.sticky-nav').removeClass("is-sticky");
 		}
+	});
+});
+
+// 2) Hide columns
+$(document).ready(function(){
+	$(".showSize").click(function(){
+		$(this).toggleClass("active");
+		if($(this).hasClass("active"))
+			$(".size").show();
+		else
+			$(".size").hide();
+	});
+	$(".showType").click(function(){
+		$(this).toggleClass("active");
+		if($(this).hasClass("active"))
+			$(".mimetype").show();
+		else
+			$(".mimetype").hide();
+	});
+	$(".showCreation").click(function(){
+				$(this).toggleClass("active");
+		if($(this).hasClass("active"))
+			$(".creation_date").show();
+		else
+			$(".creation_date").hide();
 	});
 });
